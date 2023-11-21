9a87ae5ac74ea243e1d4115f6d0cdc203ea9a607
dantzs <marcelohrs2005@gmail.com>
2023-11-21 19:52:21 -0300

    chore: configuração do adminJs e estilização

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import express from "express";
 import { sequelize } from "./database";
+import { AdminJsRouter, adminJs } from "./adminJs";
 
 const app = express()
+
+app.use(adminJs.options.rootPath, AdminJsRouter)
+
+app.use(express.static('public'))
 
 const PORT = process.env.PORT || 8080;
 
