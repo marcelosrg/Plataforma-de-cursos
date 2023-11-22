a322d6172f10c6c03052cca3f153cffc5c32ed9c
dantzs <marcelohrs2005@gmail.com>
2023-11-22 14:35:09 -0300

    feat: criação da tabela epsiodios e config de upload de eps

---
+++ src/adminJs/resources/episode.ts
@@ -0,0 +1,30 @@
+import uploadFileFeature from "@adminjs/upload";
+import { FeatureType, ResourceOptions } from "adminjs";
+import path, { dirname } from "path";
+
+export const episodeResourceOptions: ResourceOptions = {
+  navigation: 'Catálogo',
+  editProperties: ['name', 'synopsis', 'courseId', 'order','uploadVideo', 'secondsLong'],
+  filterProperties: ['name', 'synopsis', 'courseId','secondsLong', 'createdAt', 'updatedAt'],
+  listProperties: ['id', 'name', 'courseId', 'order', 'secondslong'],
+  showProperties: ['id', 'name', 'synopsis','courseId', 'order','videoUrl','secondsLong', 'createdAt', 'updatedAt']
+
+}
+
+export const episodeResourceFeatures: FeatureType[] = [
+  uploadFileFeature({
+    provider: {
+      local: {
+        bucket: path.join( __dirname, '..', '..', '..', 'uploads')
+      }
+    },
+
+    properties: {
+      key: 'videoUrl',
+      file: 'uploadVideo'
+    },
+
+    uploadPath: (record, filename) => `videos/course ${record.get('courseId')}/${filename}`
+    
+  })
+]
--- src/adminJs/resources/index.ts
+++ src/adminJs/resources/index.ts
@@ -1,7 +1,8 @@
 import { ResourceWithOptions } from "adminjs";
-import { Category, Course } from "../../models";
+import { Category, Course, Episode } from "../../models";
 import { categoryResourceOptions } from "./category";
 import { courseResourceOptions} from "./course"
+import { episodeResourceFeatures, episodeResourceOptions } from "./episode";
 
 export const adminJsResources: ResourceWithOptions[] = [
   {
@@ -11,6 +12,11 @@ export const adminJsResources: ResourceWithOptions[] = [
   {
     resource: Course,
     options: courseResourceOptions
+  },
+  {
+    resource: Episode,
+    options: episodeResourceOptions,
+    features: episodeResourceFeatures
   }
 ]
 
