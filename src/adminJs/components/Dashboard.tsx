3e14ce0a2210c0f40bd583d68bb3291e8544fe1a
dantzs <marcelohrs2005@gmail.com>
2023-11-22 16:26:32 -0300

    feat: estilização do painel admin

---
+++ src/adminJs/components/Dashboard.tsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+import {
+  H1,
+  H2,
+  Table,
+  TableBody,
+  TableCell,
+  TableRow,
+  TableHead,
+} from "@adminjs/design-system";
+import { ApiClient } from "adminjs";
+
+export default function Dashboard() {
+  const api = new ApiClient();
+  const [resources, setResources] = useState<{ [key: string]: number }>();
+  useEffect(() => {
+    fetchDashboardData();
+  });
+
+  async function fetchDashboardData() {
+    const res = await api.getDashboard();
+    setResources(res.data);
+  }
+  return (
+    <section style={{ padding: "1.5rem" }}>
+      <H1>Seja bem-vindo!</H1>
+
+      <section style={{ backgroundColor: "#FFF", padding: "1.5rem" }}>
+        <H2>Resumo</H2>
+        <Table>
+          <TableHead>
+            <TableRow style={{ backgroundColor: "#FF0043" }}>
+              <TableCell style={{ color: "#FFF" }}>Recurso</TableCell>
+              <TableCell style={{ color: "#FFF" }}>Registros</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {resources ? (
+              Object.entries(resources).map(([resource, count]) => (
+                <TableRow key={resource}>
+                  <TableCell>{resource}</TableCell>
+                  <TableCell>{count}</TableCell>
+                </TableRow>
+              ))
+            ) : (
+              <></>
+            )}
+          </TableBody>
+        </Table>
+      </section>
+    </section>
+  );
+}
