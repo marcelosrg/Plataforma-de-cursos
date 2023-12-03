800c90afcb8ddfe75c74ff89c486b58a3beb54dd
dantzs <marcelohrs2005@gmail.com>
2023-12-02 22:59:02 -0300

    feat: obtendo os 10 cursos mais populares

--- src/controllers/coursesController.ts
+++ src/controllers/coursesController.ts
@@ -30,6 +30,18 @@ export const coursesController = {
     }
   },
 
+  //GET /courses/popular
+  pupolarCourse: async(req: Request, res: Response) => {
+    try {
+      const topTen = await courseService.getTopTenByLikes();
+      return res.json(topTen)
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message });
+      }
+    }
+  },
+
   //GET /courses/search
 
   search: async (req: Request, res: Response) => {
--- src/services/coursesService.ts
+++ src/services/coursesService.ts
@@ -1,85 +1,96 @@
-import { Op } from "sequelize"
-import { Course } from "../models"
+import { Op } from "sequelize";
+import { Course } from "../models";
 
 export const courseService = {
-     findByIdWithEpisodes: async (id: string) => {
-          const courseWithEpisodes = await Course.findByPk(id, {
-               attributes: [
-                    'id',
-                    'name',
-                    'synopsis',
-                    ['thumbnail_url', 'thumbnailUrl']
-               ],
+  findByIdWithEpisodes: async (id: string) => {
+    const courseWithEpisodes = await Course.findByPk(id, {
+      attributes: ["id", "name", "synopsis", ["thumbnail_url", "thumbnailUrl"]],
 
-               include: {
-                    association: 'episodes', 
-                    attributes: [
-                         'id',
-                         'name',
-                         'synopsis',
-                         'order',
-                         ['video_url', 'videoUrl'],
-                         ['seconds_long', 'secondsLong']
-                    ],
-                    order: [['order', 'ASC']],
-                    separate: true
-               }
-          })
-           return courseWithEpisodes
-     },
+      include: {
+        association: "episodes",
+        attributes: [
+          "id",
+          "name",
+          "synopsis",
+          "order",
+          ["video_url", "videoUrl"],
+          ["seconds_long", "secondsLong"],
+        ],
+        order: [["order", "ASC"]],
+        separate: true,
+      },
+    });
+    return courseWithEpisodes;
+  },
 
+  getRandomFeaturedCourses: async () => {
+    const featuredCourses = await Course.findAll({
+      attributes: ["id", "name", "synopsis", ["thumbnail_url", "thumbnailUrl"]],
+      where: {
+        featured: true,
+      },
+    });
 
-     getRandomFeaturedCourses: async () => {
-          const featuredCourses = await Course.findAll({
-               attributes: [
-                    'id',
-                    'name',
-                    'synopsis',
-                    ['thumbnail_url', 'thumbnailUrl']
-               ],
-               where:  {
-                    featured: true
-               }
-          })
+    const randomFeaturedCourses = featuredCourses.sort(
+      () => 0.5 - Math.random()
+    );
 
-          const randomFeaturedCourses = featuredCourses.sort(() => 0.5 - Math.random())
+    return randomFeaturedCourses.slice(0, 3);
+  },
 
-          return randomFeaturedCourses.slice(0, 3)
-     },
+  getToptenNewest: async () => {
+    const courses = await Course.findAll({
+      limit: 10,
+      order: [["created_at", "DESC"]],
+    });
 
-     getToptenNewest: async () => {
-          const courses = await Course.findAll({
-               limit: 10,
-               order: [['created_at', 'DESC']]
-          })
+    return courses;
+  },
 
-          return courses
-     },
+  getTopTenByLikes: async () => {
+    const result = await Course.sequelize?.query(
+      `SELECT
+        courses.id,
+        courses.name,
+        courses.synopsis,
+        courses.thumbnail_url as thumbnailUrl,
+        COUNT(users.id) AS likes
+      FROM courses
+        LEFT OUTER JOIN likes
+          ON courses.id = likes.course_id
+          INNER JOIN users
+            ON users.id = likes.user_id
+      GROUP BY courses.id
+      ORDER BY likes DESC
+      LIMIT 10;`
+    );
 
+    if (result) {
+      const [topTen] = result;
+      return topTen;
+    } else {
+      return null;
+    }
+  },
 
-     findByName: async(name: string, page: number, perPage: number) => {
-          const offset = (page -1) * perPage
-          const {count, rows} = await Course.findAndCountAll({
-               attributes: [
-                    'id',
-                    'name',
-                    'synopsis',
-                    ['thumbnail_url', 'thumbnailUrl']
-               ],
-               where: {
-                    name: {
-                         [Op.iLike]: `%${name}%`
-                    }
-               },
-               limit: perPage,
-               offset
-          })
+  findByName: async (name: string, page: number, perPage: number) => {
+    const offset = (page - 1) * perPage;
+    const { count, rows } = await Course.findAndCountAll({
+      attributes: ["id", "name", "synopsis", ["thumbnail_url", "thumbnailUrl"]],
+      where: {
+        name: {
+          [Op.iLike]: `%${name}%`,
+        },
+      },
+      limit: perPage,
+      offset,
+    });
 
-          return {
-               courses: rows,
-               page,
-               perPage,
-               total: count
-          }
-     }
-}
+    return {
+      courses: rows,
+      page,
+      perPage,
+      total: count,
+    };
+  },
+};
